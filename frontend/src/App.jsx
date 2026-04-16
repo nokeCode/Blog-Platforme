1655921cd63c078310f43c79eaa3790b8777cddd
nokeCode <nokegermain@gmail.com>
2026-04-16 17:56:33 +0000

    feat: display posts from API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-function App(){
-  return(
+import { useEffect, useState } from "react";
+import API from "./services/api";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    API.get("/posts")
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
     <div>
-      <h1>Blog platform</h1>
+      <h1>Blog Platform</h1>
+
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
